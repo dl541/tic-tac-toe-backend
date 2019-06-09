66f5ac1390f8ec78d7049cbfd386cfe39194d522
dl541 <dl541@cam.ac.uk>
2019-06-09 18:29:56 +0100

    Add function for count states in rows, columns and diagonals

--- GameBoard.js
+++ GameBoard.js
@@ -4,9 +4,15 @@ class GameBoard {
   constructor() {
     this.length = 3;
     this.grids = this.constructBoardGrids();
-    this.stateCountInRows = [...Array(this.length).keys()].map(index => new StateCounter());
-    this.stateCountInCols = [...Array(this.length).keys()].map(index => new StateCounter());
-    this.stateCountInDiags =[...Array(2).keys()].map(index => new StateCounter());
+    this.stateCountInRows = [...Array(this.length).keys()].map(
+      index => new StateCounter()
+    );
+    this.stateCountInCols = [...Array(this.length).keys()].map(
+      index => new StateCounter()
+    );
+    this.stateCountInDiags = [...Array(2).keys()].map(
+      index => new StateCounter()
+    );
   }
 
   constructBoardGrids() {
@@ -20,33 +26,67 @@ class GameBoard {
     return rows;
   }
 
+  makeMove(gridState, coordinate) {
+    this.setGridState(gridState, coordinate);
+    this.updateStateCount(gridState, coordinate);
+  }
+
   setGridState(gridState, coordinate) {
-    var [ r, c ] = coordinate;
-    console.log(r, c);
+    var [r, c] = coordinate;
     this.grids[r][c].state = gridState;
   }
 
-  isWinner(gridState){
-      checkIfWinByRows(gridState)
-      checkIfWinByCols(gridState)
-      checkIfWinByDiags(gridState)
+  updateStateCount(gridState, coordinate) {
+    var [r, c] = coordinate;
+    this.updateStateCountInRows(gridState, r);
+    this.updateStateCountInCols(gridState, c);
+    this.updateStateCountInDiags(gridState, coordinate);
   }
 
-  checkIfWinByRows(gridState){
-      for (var r = 0; r < this.length; r++){
-          this.checkIfWinByRows()
-      }
+  updateStateCountInRows(gridState, row) {
+    var currentCount = this.stateCountInRows[row].get(gridState);
+    this.stateCountInRows[row].set(gridState, currentCount + 1);
   }
 
-  checkIfWinByRow(){
-
+  updateStateCountInCols(gridState, col) {
+    var currentCount = this.stateCountInCols[col].get(gridState);
+    this.stateCountInCols[col].set(gridState, currentCount + 1);
   }
+
+  updateStateCountInDiags(gridState, coordinate) {
+    var [r, c] = coordinate;
+    if (r == c) {
+        var currentCount = this.stateCountInDiags[0].get(gridState);
+      this.stateCountInDiags[0].set(gridState, currentCount + 1);
+    }
+
+    if (r == this.length - 1 - c) {
+        var currentCount = this.stateCountInDiags[1].get(gridState);
+      this.stateCountInDiags[1].set(gridState, currentCount + 1);
+    }
+  }
+
+  isWinner(gridState) {
+    checkIfWinByRows(gridState);
+    checkIfWinByCols(gridState);
+    checkIfWinByDiags(gridState);
+  }
+
+  checkIfWinByRows(gridState) {
+    for (var r = 0; r < this.length; r++) {
+      this.checkIfWinByRows();
+    }
+  }
+
+  checkIfWinByRow() {}
 }
 
-class StateCounter extends Map{
-    constructor(){
-        super([[GridState.CROSS, 0],[GridState.NAUGHT, 0]]);
-    }
+class StateCounter extends Map {
+  constructor() {
+    super();
+    this.set(GridState.NAUGHT, 0);
+    this.set(GridState.CROSS, 0);
+  }
 }
 
 GameBoard.prototype.toString = function() {
@@ -61,6 +101,8 @@ GameBoard.prototype.toString = function() {
 
 var board = new GameBoard();
 console.log(board.toString());
-board.setGridState(GridState.CROSS, [1,2]);
-console.log(board.stateCountInRows);
+board.makeMove(GridState.CROSS, [1, 2]);
+console.log(board.stateCountInRows, board.stateCountInCols, board.stateCountInDiags);
 
+board.makeMove(GridState.NAUGHT, [1, 1]);
+console.log(board.stateCountInRows, board.stateCountInCols, board.stateCountInDiags);
